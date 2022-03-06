4326120056334566276897e637a896d5dae78c76
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2022-03-06 19:46:19 +0530

    Update Gmaps.jsx

--- src/Components/Gmaps.jsx
+++ src/Components/Gmaps.jsx
@@ -1,14 +1,14 @@
 const GMap=({city})=>{
-    console.log(city)
-    return (
-    <div className="map" >
-       <div >
-        <div id="canvasfor-googlemap">
-            <iframe id="maps" src={`https://www.google.com/maps/embed/v1/search?q=${city}&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`} frameBorder="0" ></iframe>
-        </div>
-        <a className="googlemaps-html" href="https://www.embed-map.com" id="grabmap-authorization">https://www.embed-map.com</a>
+  console.log(city)
+  return (
+  <div className="map" >
+     <div >
+      <div id="canvasfor-googlemap">
+          <iframe id="maps" src={`https://www.google.com/maps/embed/v1/search?q=${city}&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`} frameBorder="0" ></iframe>
       </div>
-   </div>
- )
+      <a className="googlemaps-html" href="https://www.embed-map.com" id="grabmap-authorization">https://www.embed-map.com</a>
+    </div>
+ </div>
+)
 }
 export default GMap
