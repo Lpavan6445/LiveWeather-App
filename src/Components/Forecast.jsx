208fd012b28f4c8464d0e4aff9669a66a1f660fd
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2022-03-06 19:04:27 +0530

    done

---
+++ src/Components/Forecast.jsx
@@ -0,0 +1,15 @@
+const Forecast=({forecastData,getDayName})=>{
+    // console.log(forecastData)
+    const dayIndex = new Date().getDay();
+    return(
+        <div className="forecast">
+           {forecastData.length>0? forecastData.map((data)=><div className="subforcast"> 
+                <p className="forecastDay">{getDayName(dayIndex)}</p>
+                
+                <img className="forecastIcon" src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}/>
+                <p className="forecastTemp">{`${Math.floor(data.main.temp)}°C`}</p>
+            </div>):""}
+        </div>
+    )
+}
+export default Forecast
