ae2f7e62fdbbc71a848cd2c46d2b06c7a7a40032
PAVAN LENKALA <87610669+Lpavan6445@users.noreply.github.com>
2022-03-30 09:38:38 +0530

    Update Forecast.jsx

--- src/Components/Forecast.jsx
+++ src/Components/Forecast.jsx
@@ -3,8 +3,8 @@ const Forecast=({forecastData,getDayName})=>{
     const dayIndex = new Date().getDay();
     return(
         <div className="forecast">
-           {forecastData.length>0? forecastData.map((data)=><div className="subforcast"> 
-                <p className="forecastDay">{getDayName(dayIndex)}</p>
+           {forecastData.length>0? forecastData.map((data,i)=><div className="subforcast"> 
+                <p className="forecastDay">{getDayName(dayIndex+i)}</p>
                 
                 <img className="forecastIcon" src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}/>
                 <p className="forecastTemp">{`${Math.floor(data.main.temp)}°C`}</p>
